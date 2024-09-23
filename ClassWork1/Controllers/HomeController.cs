36269fb887ef51eaccad0c6695e54f2498d9b986
D0KVA <isip_v.s.sokolov@mpt.ru>
2024-09-23 22:34:44 +0300

    asdasd

---
+++ ClassWork1/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using ClassWork1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ClassWork1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+		public IActionResult Page4()
+		{
+			return View();
+		}
+
+		public IActionResult Catalog()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
